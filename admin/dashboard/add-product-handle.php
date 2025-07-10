b61500925ff95445004fce74cc5362dfd20d09ed
Aayush Shrestha <learn.aayush@gmail.com>
2025-07-10 18:11:23 +0545

    add product made

---
+++ admin/dashboard/add-product-handle.php
@@ -0,0 +1,42 @@
+<?php 
+
+if($_SERVER['REQUEST_METHOD'] != 'POST'){
+    header('Location: dashboard.php');
+    exit();
+}
+$product_image = $_FILES["image"];
+$product_name = $_POST['name'];
+$product_description = $_POST['description'];
+$price = $_POST['price'];
+$image_path = "";
+
+var_dump($_FILES);
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+ die;
+if(isset($product_image) && $product_image['error'] == 0){
+    $upload_dir = "uploads/";
+    $image_name = time() . '_' . basename($author_image['name']);
+    $target_path = $upload_dir . $image_name;
+
+    if(move_uploaded_file($author_image['tmp_name'], $target_path )){
+        $image_path  = $target_path;
+    }
+}
+
+include '../db-conn.php';
+
+
+$query = "INSERT INTO menu (image-url, item-name, description, price) VALUES (?, ?, ?)";
+$abc = mysqli_prepare($conn, $query);
+mysqli_stmt_bind_param($abc, "sss", $quote, $author_name, $image_path);
+mysqli_stmt_execute($abc);
+
+
+header("Location: dashboard.php?path=menu");
+
+
+
+
+?>
